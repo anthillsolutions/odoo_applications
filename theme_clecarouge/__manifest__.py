e10136ae6fbc69f1163d0e0645df0143c50f1286
Pierre Repetto-Andipatin <pierre@anthillsolutions.fr>
2023-06-26 12:22:30 +0200

    bump v0.5

--- theme_clecarouge/__manifest__.py
+++ theme_clecarouge/__manifest__.py
@@ -10,7 +10,7 @@
   'depends': ['website_sale'],
   'auto_install': False,
   'installable': True,
-  'version': '0.4',
+  'version': '0.5',
   'data': [ 
     'views/contact_us.xml', 
     'views/footer.xml',
